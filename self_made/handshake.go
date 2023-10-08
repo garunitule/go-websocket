de1dd36af2bb5787d1a4f8b9dae51769caa6c06a
garunitule <youtaiguhe@gmail.com>
2023-10-08 21:05:26 +0900

    レスポンス返す関数を別ファイルに切り出す

--- self_made/handshake.go
+++ self_made/handshake.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bufio"
+	"crypto/sha1"
+	b64 "encoding/base64"
 	"fmt"
 	"net/http"
 )
@@ -24,4 +27,21 @@ func validateHandShakeReq(r *http.Request, w http.ResponseWriter) error {
 		return err
 	}
 	return nil
+}
+
+func openHandShake(r *http.Request, buf *bufio.ReadWriter) *bufio.ReadWriter {
+	key := r.Header.Get("Sec-WebSocket-Key")
+	h := sha1.New()
+	// TODO: 定数で良い理由を調査
+	h.Write([]byte(key + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"))
+	accept := b64.StdEncoding.EncodeToString(h.Sum(nil))
+	fmt.Fprintf(buf, "HTTP/1.1 101 Switching Protocols\r\n")
+	fmt.Fprintf(buf, "Upgrade: websocket\r\n")
+	fmt.Fprintf(buf, "Connection: Upgrade\r\n")
+	fmt.Fprintf(buf, "Sec-WebSocket-Accept: %s\r\n", accept)
+	if r.Header.Get("Sec-WebSocket-Protocol") != "" {
+		fmt.Fprintf(buf, "Sec-WebSocket-Protocol: %s\r\n", r.Header.Get("Sec-WebSocket-Protocol"))
+	}
+	fmt.Fprintf(buf, "\r\n")
+	return buf
 }
--- self_made/main.go
+++ self_made/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"crypto/sha1"
-	b64 "encoding/base64"
 	"fmt"
 	"log"
 	"net"
@@ -47,21 +45,7 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// implement the rest of the handshake
-	key := r.Header.Get("Sec-WebSocket-Key")
-	h := sha1.New()
-	// TODO: 定数で良い理由を調査
-	h.Write([]byte(key + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"))
-	accept := b64.StdEncoding.EncodeToString(h.Sum(nil))
-	fmt.Fprintf(buf, "HTTP/1.1 101 Switching Protocols\r\n")
-	fmt.Fprintf(buf, "Upgrade: websocket\r\n")
-	fmt.Fprintf(buf, "Connection: Upgrade\r\n")
-	fmt.Fprintf(buf, "Sec-WebSocket-Accept: %s\r\n", accept)
-	if r.Header.Get("Sec-WebSocket-Protocol") != "" {
-		fmt.Fprintf(buf, "Sec-WebSocket-Protocol: %s\r\n", r.Header.Get("Sec-WebSocket-Protocol"))
-	}
-	fmt.Fprintf(buf, "\r\n")
-
+	buf = openHandShake(r, buf)
 	buf.Flush()
 	go handleConnection(conn)
 }
